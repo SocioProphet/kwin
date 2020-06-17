e08b9281ee258c4d80dae6f3281fe9377006b4f9
Carson Black <uhhadd@gmail.com>
2020-06-17 12:26:21 -0400

    Handle change in corresponding kwayland-server patch

--- xdgshellclient.cpp
+++ xdgshellclient.cpp
@@ -1778,7 +1778,7 @@ XdgPopupClient::XdgPopupClient(XdgPopupInterface *shellSurface)
             this, &XdgPopupClient::handleGrabRequested);
     connect(shellSurface, &XdgPopupInterface::initializeRequested,
             this, &XdgPopupClient::initialize);
-    connect(shellSurface, &XdgPopupInterface::popupRepositionRequested,
+    connect(shellSurface, &XdgPopupInterface::repositionRequested,
             this, &XdgPopupClient::reposition);
     connect(shellSurface, &XdgPopupInterface::destroyed,
             this, &XdgPopupClient::destroyClient);
