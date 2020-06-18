353cccc8874d9bbdcbad85d1bc65f69aee366ae5
Carson Black <uhhadd@gmail.com>
2020-06-18 02:56:45 -0400

    do the disconnect dance

--- xdgshellclient.cpp
+++ xdgshellclient.cpp
@@ -1799,6 +1799,9 @@ void XdgPopupClient::handlePositionerBindings()
     if (m_shellSurface->positioner().reactive()) {
         connect(transientFor(), &AbstractClient::frameGeometryChanged,
                 this, &XdgPopupClient::relayout, Qt::UniqueConnection);
+    } else {
+        disconnect(transientFor(), &AbstractClient::frameGeometryChanged,
+                this, &XdgPopupClient::relayout);
     }
 }
 
